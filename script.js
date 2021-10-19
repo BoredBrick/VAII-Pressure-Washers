641e5bcb14b716ac2100589316a68b9dca0b5ddb
BoredBrick <67233864+BoredBrick@users.noreply.github.com>
2021-10-19 23:18:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function initMap() {
+    var coords = {lat: 48.811832, lng: 19.652496 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: coords,
+        scrollwheel: false
+    });
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        label: "PowerWashers"
+    });
+  }
